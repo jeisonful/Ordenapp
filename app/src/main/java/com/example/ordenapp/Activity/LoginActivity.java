833e4c7fb094bae7c33d0f6c790c6c673add5ad2
Jeison Martinez <jeisonful@gmail.com>
2024-04-27 20:38:15 -0400

    Initial Commit

---
+++ app/src/main/java/com/example/ordenapp/Activity/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.example.ordenapp.Activity;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.ordenapp.R;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
