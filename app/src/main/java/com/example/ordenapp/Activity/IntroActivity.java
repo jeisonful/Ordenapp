c1f15256816220458a6b49b3bc024229891869ff
Jeison Martinez <jeisonful@gmail.com>
2024-05-13 11:21:53 -0400

    Initial Commit

--- app/src/main/java/com/example/ordenapp/Activity/IntroActivity.java
+++ app/src/main/java/com/example/ordenapp/Activity/IntroActivity.java
@@ -3,6 +3,8 @@ package com.example.ordenapp.Activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+
 
 import com.example.ordenapp.databinding.ActivityIntroBinding;
 import com.google.firebase.auth.FirebaseAuth;
@@ -15,23 +17,25 @@ ActivityIntroBinding binding;
         binding = ActivityIntroBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         getWindow().getDecorView().setBackgroundColor(Color.WHITE);
-
+        getWindow().setStatusBarColor(Color.WHITE);
         setVariable();
-        getWindow().setStatusBarColor(Color.parseColor("#4682B4"));
+
     }
 
     private void setVariable() {
-        binding.btnLogin.setOnClickListener(v -> {
-            if(mAuth.getCurrentUser()!=null){
-                startActivity(new Intent(IntroActivity.this, MainActivity.class));
-            }else{
-                startActivity(new Intent(IntroActivity.this, LoginActivity.class));
-            }
-        });
 
-        binding.btnRegister.setOnClickListener(v ->{
-            FirebaseAuth.getInstance().signOut();
-            startActivity(new Intent(IntroActivity.this, RegisterActivity.class));
-        });
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if(mAuth.getCurrentUser()!=null){
+                    startActivity(new Intent(IntroActivity.this, MainActivity.class));
+                }else{
+                    startActivity(new Intent(IntroActivity.this, LoginActivity.class));
+                }
+            }
+        }, 2500);
+
+
+
     }
 }
